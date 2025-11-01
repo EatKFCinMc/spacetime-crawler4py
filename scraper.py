c6272ff90fd64e0a00a7142f99ef6dae2501a6c8
Arch <yuhaow29@uci.edu>
2025-11-01 04:35:52 -0700

    commit

--- scraper.py
+++ scraper.py
@@ -63,11 +63,11 @@ def extract_next_links(url, resp):
     for match in url_pattern.findall(text):
         urls_before_process.append(match[0] or match[1])
 
-    print(urls_before_process)
+    # print(urls_before_process)
 
     valid_domains = ("ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu")
     for link in urls_before_process:
-        parsed = urlparse(link)
+        # parsed = urlparse(link)
 
         if re.search(r"([?&]page=\d+)|([?&]session)|([?&]sid=)|(\d{4}/\d{2}/\d{2})", link, re.I):
             continue
