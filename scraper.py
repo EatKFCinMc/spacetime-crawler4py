b7dfcfc6d127a650e59db80c7caae682d7586709
Arch <yuhaow29@uci.edu>
2025-11-01 05:17:13 -0700

    commit

--- scraper.py
+++ scraper.py
@@ -67,9 +67,9 @@ def is_valid(url):
         if not any(parsed.hostname.endswith(domain) for domain in domains):
             return False
 
-        # loops = (r'page=\d{3,}', r'offset=\d{3,}', r'sessionid=\w{10,}', r'\d{10,}')
-        # if any(re.search(p, url.lower()) for p in loops):
-        #     return False
+        loops = (r'page=\d{3,}', r'offset=\d{3,}', r'sessionid=\w{10,}', r'\d{10,}')
+        if any(re.search(p, url.lower()) for p in loops):
+            return False
 
         # query = ("/event")
         # if any(re.search(p, url.lower()) for p in query):
