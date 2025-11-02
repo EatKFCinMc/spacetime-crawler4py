a4f0365d5484b961036af4430ee46a8e51c2b5fd
Arch <yuhaow29@uci.edu>
2025-11-01 17:59:29 -0700

    commit

--- scraper.py
+++ scraper.py
@@ -39,6 +39,7 @@ def extract_next_links(url, resp):
     urls = [a['href'] for a in html.find_all('a', href=True)]
     text = html.get_text()
 
+    print(len(text) / len(resp.raw_response.text))
     if len(text) / len(resp.raw_response.text) < 0.1:
         return []
 
